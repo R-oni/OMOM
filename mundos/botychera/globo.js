e260d806fde92bcc625d5128e639c7d4b788a595
R-oni <99811214+R-oni@users.noreply.github.com>
2025-02-26 15:36:09 -0300

    Update globo.js

--- mundos/botychera/globo.js
+++ mundos/botychera/globo.js
@@ -62,8 +62,8 @@ function initGlobe() {
   scene.add(cloudMesh);
 
   // Inclinação para melhorar a visualização do anel
-  sphere.rotation.x = Math.PI / 6; // ~30° de inclinação
-  cloudMesh.rotation.x = Math.PI / 6; // Acompanha a inclinação
+  sphere.rotation.x = Math.PI / 3; // ~30° de inclinação
+  cloudMesh.rotation.x = Math.PI / 3; // Acompanha a inclinação
 
   // (4) Luz ambiente + direcional
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
